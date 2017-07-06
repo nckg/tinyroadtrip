96d854b71e06a915efadc9261c0718ad3ed3c563
Nick Goris <nick@robarov.be>
2017-07-06 14:15:37 +0200

    cron job

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-#!/usr/bin/env node
+var friendlyCron = require('friendly-cron');
 var run = require('./lib/index');
+var cron = require('node-cron');
 
-run();
+var pattern = friendlyCron('every 4 hours');
+
+cron.schedule(pattern, function(){
+    run();
+});
