a8b419a385a9fe553d1d5d1fa31090eeb20e17d3
Nick Goris <nick@robarov.be>
2017-07-06 21:46:44 +0200

    add micro for deployment on now

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var cron = require('node-cron');
 
 var pattern = friendlyCron('every 4 hours');
 
-cron.schedule(pattern, function(){
-    run();
-});
+const go = () => {
+    cron.schedule(pattern, function(){
+        run();
+    });
+}
+
+module.exports = (req, res) => {
+    go();
+    res.end('Welcome to Micro')
+}
